ca3d47b1ce59d12b6185eb3e9df801bddf0d8f4b
keri19372 <keri19372@gmail.com>
2019-11-02 23:43:46 +0200

    make html css and  background

---
+++ script/script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+const graund = new Image();
+graund.src = "img/graund.png";
+
+const food = new Image();
+food.src = "img/food.png";
